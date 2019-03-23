a1ed415115658a6766992e7befbc2edad9eeae32
Alexandre <alexmilo96@wanadoo.fr>
2019-03-23 13:48:52 +0100

    Task 4-5

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function init() {
 	image.src="flower2.png";
 	document.getElementById("new_element").appendChild(image);
 
+	let spans = document.getElementById("rainbow").getElementsByTagName("span");
+	let colors = ["red", "orange"];
+
+	for (var i = spans.length - 1; i >= 0; i--) {
+		spans[i].style.color = colors[Math.round(Math.random())];
+	}
+
 }
