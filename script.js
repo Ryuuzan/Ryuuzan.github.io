de610818b280c907350bac8c2793703b040df24c
Alexandre <alexmilo96@wanadoo.fr>
2019-03-24 15:03:28 +0100

    Shopping.html without rm button

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function init() {
 	var changeSrc = function(event) {
 		let imgs = document.getElementById("event").getElementsByTagName("img")
 		for (var i = imgs.length - 1; i >= 0; i--) {
-		imgs[i].src="flower1.png";
+			imgs[i].src="flower1.png";
 		}
 		if (event.target.src) {
 			event.target.src = "flower2.png";
@@ -23,4 +23,15 @@ function init() {
 	};
 
 	document.getElementById("event").addEventListener("mouseover", changeSrc);
+}
+
+function addItem(){
+	let c1 = document.getElementById("check1");
+	let c2 = document.getElementById("check2");
+	let list = document.createElement("li");
+	let element = document.getElementById("name").value;
+	list.appendChild(document.createTextNode(element));
+	if(c1.checked){list.style.textDecoration = "underline";}
+	if(c2.checked){list.style.color = "red";}
+	document.getElementById("mylist").appendChild(list);
 }
